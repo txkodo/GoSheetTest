958fd173ef9bbb687b324fa548bacef33d57fc6b
txkodo <kodo.hsmkai@gmail.com>
2024-04-15 23:30:40 +0900

    🐛 fixed base path

--- website/svelte.config.js
+++ website/svelte.config.js
@@ -1,6 +1,8 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
+const dev = process.argv.include('dev');
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
@@ -12,8 +14,11 @@ const config = {
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter({
-			fallback:"index.html"
-		})
+			fallback: 'index.html'
+		}),
+		paths: {
+			base: dev ? '' : 'GoSheetTest/'
+		}
 	}
 };
 
